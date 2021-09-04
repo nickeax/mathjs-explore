b2be14285cf9ff65b0e7e4705c9e9adec7f44e69
Nick Fletcher <nickeax@gmail.com>
2021-09-04 12:32:24 +1000

    Complete simple matrix multiplication

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
-const mA = math.matrix([[1, 2], [3, 4], [5, 6]])
-const mB = math.matrix([[1, -1], [2, -2], [3, -3]])
-const matrixAdd = math.add(mA, mB)
+const rosesPrice = math.matrix([[3, 4, 2]])
+const rosesDaysSold = math.matrix([[120, 80, 60], [90, 70, 40], [60, 40, 20]])
 
-const mC = math.matrix([[1, 2], [3, 4], [5, 6]])
-const mD = math.matrix([[1, -1], [2, -2], [3, -3]])
-const matrixSub = math.subtract(mC, mD)
-
-console.log(`Matrix A: ${mA}`)
-console.log(`Matrix A X 3: ${math.multiply(3, mA)}`)
-
-// console.log(`Matrix Add: ${matrixAdd}`)
-// console.log(`Matrix C: ${mC}`)
-// console.log(`Matrix D: ${mD}`)
-// console.log(`Matrix Sub: ${matrixSub}`)
-// console.table(mA._data)
+console.log(`Rose Prices Matrix: ${rosesPrice}\nRoses Sold Per Day: ${rosesDaysSold}`)
+console.log(`Results: ${math.multiply(rosesPrice, rosesDaysSold)}`)
